7543d8f00c41f8cb7a0dcaca05c8bdece0c2dbad
정용성 <jys9049@blocko.io>
2025-05-20 22:19:44 +0900

    feat: 프로젝트 세팅

---
+++ next.config.ts
@@ -0,0 +1,44 @@
+import type { NextConfig } from "next";
+import path from "path";
+
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const nextConfig: NextConfig = {
+  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: "http",
+        hostname: "k.kakaocdn.net",
+        port: "",
+        pathname: "/dn/**",
+        search: "",
+      },
+    ],
+  },
+  sassOptions: {
+    includePaths: [path.join(__dirname, "styles")],
+  },
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: ["@svgr/webpack"],
+    });
+    return config;
+  },
+  experimental: {
+    turbo: {
+      rules: {
+        "*.svg": {
+          loaders: ["@svgr/webpack"],
+          as: "*.ts",
+        },
+      },
+    },
+  },
+};
+
+export default nextConfig;
