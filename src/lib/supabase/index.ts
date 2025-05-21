f7a746c3126d64bf5a97dcc17e8211fd951ff94f
정용성 <jys9049@blocko.io>
2025-05-21 14:30:55 +0900

    feat: supabase server 변수 따로 관리

--- src/lib/supabase/index.ts
+++ src/lib/supabase/index.ts
@@ -2,8 +2,13 @@ import { createClient } from "@supabase/supabase-js";
 
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL || "";
 const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || "";
+
+const supabaseUrl_server = process.env.UPABASE_URL || "";
 const supabaseServiceRoleKey = process.env.SUPABASE_SERVICE_ROLE_KEY || "";
 
 const supabase = createClient(supabaseUrl, supabaseKey);
-const server_supabase = createClient(supabaseUrl, supabaseServiceRoleKey);
+const server_supabase = createClient(
+  supabaseUrl_server,
+  supabaseServiceRoleKey
+);
 export { supabase, server_supabase };
