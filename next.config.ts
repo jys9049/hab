527545832ce646fdd7c64781f9e6b84059c431a9
정용성 <jys9049@blocko.io>
2025-05-21 16:17:37 +0900

    feat: 원복

--- next.config.ts
+++ next.config.ts
@@ -7,21 +7,6 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 const nextConfig: NextConfig = {
-  env: {
-    NEXT_PUBLIC_BASE_URL: process.env.NEXT_PUBLIC_BASE_URL,
-    NEXT_PUBLIC_KAKAO_INTERGRITY: process.env.NEXT_PUBLIC_KAKAO_INTERGRITY,
-    NEXT_PUBLIC_KAKAO_JS_KEY: process.env.NEXT_PUBLIC_KAKAO_JS_KEY,
-    NEXT_PUBLIC_SUPABASE_URL: process.env.NEXT_PUBLIC_SUPABASE_URL,
-    NEXT_PUBLIC_SUPABASE_ANON_KEY: process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY,
-    KAKAO_CLIENT_ID: process.env.KAKAO_CLIENT_ID,
-    KAKAO_REDIRECT_URI: process.env.KAKAO_REDIRECT_URI,
-    KAKAO_SECRET_KEY: process.env.KAKAO_SECRET_KEY,
-    BASE_URL: process.env.BASE_URL,
-    JWT_ACCESS_SECRET: process.env.JWT_ACCESS_SECRET,
-    JWT_REFRESH_SECRET: process.env.JWT_REFRESH_SECRET,
-    SUPABASE_URL: process.env.SUPABASE_URL,
-    SUPABASE_SERVICE_ROLE_KEY: process.env.SUPABASE_SERVICE_ROLE_KEY,
-  },
   /* config options here */
   images: {
     remotePatterns: [
