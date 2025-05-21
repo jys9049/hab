f7412bf48590534c32dc197bb57b3ad7342125fa
정용성 <jys9049@blocko.io>
2025-05-21 14:38:03 +0900

    fix: supabase server, client 분리

--- src/lib/supabase/index.ts
+++ src/lib/supabase/index.ts
@@ -1,14 +1,14 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL || "";
-const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || "";
-
-const supabaseUrl_server = process.env.UPABASE_URL || "";
-const supabaseServiceRoleKey = process.env.SUPABASE_SERVICE_ROLE_KEY || "";
-
-const supabase = createClient(supabaseUrl, supabaseKey);
-const server_supabase = createClient(
-  supabaseUrl_server,
-  supabaseServiceRoleKey
+const supabase = createClient(
+  process.env.NEXT_PUBLIC_SUPABASE_URL!,
+  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
 );
+
+// 서버 전용 Supabase 클라이언트
+const server_supabase = createClient(
+  process.env.SUPABASE_URL!,
+  process.env.SUPABASE_SERVICE_ROLE_KEY!
+);
+
 export { supabase, server_supabase };
