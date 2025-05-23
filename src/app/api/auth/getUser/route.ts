282e06ff3d3c522028d22bd79eb75b65f48f57d4
정용성 <jys9049@blocko.io>
2025-05-24 04:38:17 +0900

    fix: try catch 추가

--- src/app/api/auth/getUser/route.ts
+++ src/app/api/auth/getUser/route.ts
@@ -4,31 +4,39 @@ import { decode } from "jsonwebtoken";
 import { NextRequest } from "next/server";
 
 export async function GET(req: NextRequest) {
-  const token = req.cookies.get("accessToken");
+  try {
+    const token = req.cookies.get("accessToken");
 
-  if (!token) {
-    return Response.json("AccessToken이 만료되었습니다.", {
-      status: 401,
-    });
+    if (!token) {
+      return Response.json("AccessToken이 만료되었습니다.", {
+        status: 401,
+      });
+    }
+
+    const userInfo = decode(token.value) as TUserType;
+    const { data, error } = await server_supabase
+      .from("users")
+      .select("*")
+      .eq("id", userInfo.id)
+      .single();
+
+    if (error) {
+      throw new Error("DB 정보를 불러오는데 실패하였습니다.");
+    }
+
+    const resData = {
+      id: data.id,
+      email: data.email,
+      nickname: data.nickname,
+      profile_img: data.profile_img,
+    };
+
+    return Response.json({ data: resData }, { status: 200 });
+  } catch (e) {
+    console.error(e);
+    return Response.json(
+      { message: "로그인에 실패하였습니다" },
+      { status: 500 }
+    );
   }
-
-  const userInfo = decode(token.value) as TUserType;
-  const { data, error } = await server_supabase
-    .from("users")
-    .select("*")
-    .eq("id", userInfo.id)
-    .single();
-
-  if (error) {
-    throw new Error("DB 정보를 불러오는데 실패하였습니다.");
-  }
-
-  const resData = {
-    id: data.id,
-    email: data.email,
-    nickname: data.nickname,
-    profile_img: data.profile_img,
-  };
-
-  return Response.json({ data: resData }, { status: 200 });
 }
