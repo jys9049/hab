c45255eaca8789c3fbfe54f25a897d8a891de536
정용성 <jys9049@blocko.io>
2025-06-08 23:06:28 +0900

    fix: 불필요한 코드 제거

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,7 +4,6 @@ import st from "@/styles/layout.module.scss";
 import KakaoLoadProvider from "@/lib/kakao/provider";
 import QueryProviders from "@/lib/tanstack/providers";
 import { ToastContainer } from "react-toastify";
-import Head from "next/head";
 
 export const metadata: Metadata = {
   metadataBase: new URL("https://hab.jeongyongseong.kr"),
